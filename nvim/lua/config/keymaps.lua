b55d50c96f5c6457973008e0c0843b31e139a5f8
Simon Semakov <simon.semakov@gmail.com>
2026-07-06 12:40:36 -0400

    Nvim, file path bindings

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Copy the current buffer's file path to the system clipboard (+ register).
+vim.keymap.set("n", "<leader>fy", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify(path, vim.log.levels.INFO, { title = "Copied absolute path" })
+end, { desc = "Copy absolute file path" })
+
+vim.keymap.set("n", "<leader>fY", function()
+  local path = vim.fn.fnamemodify(vim.fn.expand("%"), ":.")
+  vim.fn.setreg("+", path)
+  vim.notify(path, vim.log.levels.INFO, { title = "Copied relative path" })
+end, { desc = "Copy relative file path" })
