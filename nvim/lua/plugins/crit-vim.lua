c4b5d5946b7de7c09d92f6ed4c0950653669a952
Simon Semakov <simon.semakov@gmail.com>
2026-07-20 10:57:57 -0400

    Crit-vim default keybindings

--- nvim/lua/plugins/crit-vim.lua
+++ nvim/lua/plugins/crit-vim.lua
@@ -3,5 +3,8 @@ return {
     dir = vim.fn.expand("~/workspace/crit-vim"),
     name = "crit-vim",
     lazy = false, -- need VimEnter to register the socket on startup
+    config = function()
+      require("crit-vim").setup({ default_keys = true }) -- <leader>C{,C} comment operator
+    end,
   },
 }
